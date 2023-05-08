89eecae2849e6ee9c55c9fcb75e7db656084aebe
Siddhi <siddhidatrisahoo03@gmail.com>
2023-05-08 13:49:14 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,7 @@
+public class add {
+    public static void main(String[] args) {
+        int no1=10,no2=20,sum;
+        sum=no1+no2;
+        System.out.println("The sum of two numbers is "+sum);
+    }
+}
